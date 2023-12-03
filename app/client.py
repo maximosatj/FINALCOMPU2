ac7f239c431cc34073c1eca5b03787057e1562ad
Ulises <u.festin@alumno.um.edu.ar>
2023-12-03 11:55:37 -0300

    asyncio en cliente y servidor

--- app/client.py
+++ app/client.py
@@ -1,21 +1,96 @@
 import socket
 import threading
-from datetime import datetime
 from constants import HOST, PORT
+import asyncio
+import os
 
 username = input("Enter your username: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-client.send(username.encode("utf-8"))
-
 def write_messages():
     while True:
-        message_content = input('')
-        current_time = datetime.now().strftime("%H:%M:%S")
-        message = f"{current_time} {username}: {message_content}"
-        client.send(message.encode('utf-8'))
+        message_content = input("-> ")
+        if message_content == "exit":
+            client.close()
+            os._exit(0)
+        elif message_content == 'back':
+            break
+        else:
+            message = f"{username}: {message_content}"
+            client.send(message.encode('utf-8'))
 
-write_thread = threading.Thread(target=write_messages)
-write_thread.start()
+def receive_messages():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == "@username":
+                client.send(username.encode("utf-8"))
+            else:
+                print(f"\nMENSAJE DEL SERVIDOR: {message}")
+        except:
+            print("An error occurred")
+            client.close()
+            break
+
+async def compress_file():
+    print("Compressing file...")
+    await asyncio.sleep(5)
+    print("File compressed successfully")
+
+async def connect_to_server():
+    print("Establishing connection with the server...")
+    await asyncio.sleep(3)
+    print("Connected to server")
+
+async def request_users():
+    print("Requesting users...")
+    await asyncio.sleep(5)
+    print("Users received")
+
+async def send_file(file_name):
+    task1 = asyncio.create_task(compress_file())
+    task2 = asyncio.create_task(connect_to_server())
+
+    await asyncio.gather(task1, task2)
+
+    print(f"Sending file '{file_name}' ...")
+    await asyncio.sleep(5)
+    print(f"File '{file_name}' sent successfully")
+
+async def get_users_connected():
+    task1 = asyncio.create_task(connect_to_server())
+    task2 = asyncio.create_task(request_users())
+
+    await asyncio.gather(task1, task2)
+
+    client.send("get_users".encode('utf-8'))
+    await asyncio.sleep(3)
+
+
+def menu():
+    while True:
+        print("1. Send messages")
+        print("2. Send file")
+        print("3. Users connected")
+        print("4. Exit")
+        option = input("Enter an option: ")
+        if option == "1":
+            write_messages()
+        elif option == "2":
+            file = input("Enter the file name: ")
+            asyncio.run(send_file(file))
+        elif option == "3":
+            asyncio.run(get_users_connected())
+        elif option == "4":
+            client.send("".encode('utf-8'))
+            client.close()
+            os._exit(0)
+        else:
+            print("Invalid option")
+
+if __name__ == "__main__":
+    receive_thread = threading.Thread(target=receive_messages)
+    receive_thread.start()
+    menu()
--- app/server.py
+++ app/server.py
@@ -1,62 +1,108 @@
 import socket
-import threading
+import asyncio
 from datetime import datetime
-from constants import HOST, PORT 
+from constants import HOST, PORT, MAX
 from dataclasses import dataclass
+import random
+import select
 
 
-@dataclass
 class Server:
     clients = []
     usernames = []
+    chat_room = ''
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockets = [server]
 
     def connect(self):
         self.server.bind((HOST, PORT))
-        self.server.listen()
+        self.server.listen(MAX)
         print(f"Server running on {HOST}:{PORT}")
 
-    def send_to_chat_room(self, message):
-        self.clients[0].send(message)
+    async def send_to_chat_room(self, message):
+        for client in self.clients:
+            if client.username == 'chat_room':
+                print(f'Procesando mensaje {message}')
+                latency = random.uniform(0.005, 1.5)
+                await asyncio.sleep(latency)
+                client.socket.send(message)
+                print(f'Mensaje enviado {message}')
 
-    def handle_messages(self, client):
-        while True:
-            try:
-                message = client.recv(1024)
+    async def accept_connection(self):
+        # Si el socket listo es el socket del servidor, significa que hay una nueva conexión
+        client, address = self.server.accept()
+        self.sockets.append(client)
+
+        client.send("@username".encode("utf-8"))
+        username = client.recv(1024).decode('utf-8')
+
+        new_client = Client(username, client, address)
+
+        self.clients.append(new_client)
+        self.usernames.append(username)
+
+        print('Connecting...')
+        latency = random.uniform(0.005, 1.5)
+        await asyncio.sleep(latency)
+
+        print(f"{username} is connected with {str(address)}")
+
+        message = f"ChatBot: {username} joined the chat!".encode("utf-8")
+        await self.send_to_chat_room(message)
+        client.send("Connected to server".encode("utf-8"))
+
+    async def handle_message(self, s):
+        try:
+            message = s.recv(1024)
+            if message == b'get_users':
+                users = 'USERS: '+', '.join(self.users_connected())
+                s.send(users.encode('utf-8'))
+            elif message:
                 current_time = datetime.now().strftime("%H:%M:%S")
                 formatted_message = f"{current_time} {message.decode('utf-8')}"
-                self.send_to_chat_room(formatted_message.encode('utf-8'))
-            except:
-                index = self.clients.index(client)
-                username = self.usernames[index]
-                self.send_to_chat_room(f"ChatBot: {username} disconnected".encode('utf-8'))
+                await self.send_to_chat_room(formatted_message.encode('utf-8'))
+            else:
+                peername = s.getpeername()
+                print(f'El cliente {peername} se ha desconectado')
+                index = self.sockets.index(s)
+                client = self.clients[index-1]
+                await self.send_to_chat_room(f'{client.username} se ha desconectado'.encode('utf-8'))
+                s.close()
+                self.sockets.remove(s)
                 self.clients.remove(client)
-                self.usernames.remove(username)
-                client.close()
-                break
+                self.usernames.remove(client.username)
+        except Exception as e:
+            print(f'Error al recibir mensaje del cliente {e}')
+            s.close()
+            self.sockets.remove(s)
+            index = self.sockets.index(s)
+            client = self.clients[index-1]
+            self.clients.remove(client)
+            self.usernames.remove(client.username)
+            await self.send_to_chat_room(f'{client.username} se ha desconectado'.encode('utf-8'))
 
     def receive_connections(self):
+        
         while True:
-            client, address = self.server.accept()
+            # Usar select para esperar hasta que alguno de los sockets esté listo para ser leído
+            ready_to_read, _, _ = select.select(self.sockets, [], [])
+    
+            for s in ready_to_read:
+                if s is self.server:
+                    asyncio.run(self.accept_connection())
+                else:
+                    # Si el socket listo no es el socket del servidor, significa que hay un nuevo mensaje
+                    asyncio.run(self.handle_message(s))
+                    
+   
+    def users_connected(self):
+        return self.usernames
 
-            print(client, address)
-
-            client.send("@username".encode("utf-8"))
-            username = client.recv(1024).decode('utf-8')
-
-            self.clients.append(client)
-            self.usernames.append(username)
-
-            print(f"{username} is connected with {str(address)}")
-
-            message = f"ChatBot: {username} joined the chat!".encode("utf-8")
-            self.send_to_chat_room(message)
-            client.send("Connected to server".encode("utf-8"))
-
-            thread = threading.Thread(target=self.handle_messages, args=(client,))
-            thread.start()
-
-
+@dataclass
+class Client:
+    username: str
+    socket: socket.socket
+    address: str
 
 if __name__ == "__main__":
     server = Server()
